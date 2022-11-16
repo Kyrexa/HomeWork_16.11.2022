fcc400b2a77a30394697fa87c0760547cad844dc
kyrexa <arzamas.orko@yandex.ru>
2022-11-16 16:27:51 +0300

    Сделал домашнее задание 16.11.2022

---
+++ HomeWork_66/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значения M и N . Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Не использовать результирующую переменную
+// Решать через рекурсию!!!
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Clear();
+Console.WriteLine("введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат выполнения: {Sum(m, n)}");
+int Sum(int from, int to)
+{
+    if ( from > to) return Sum(to, from);
+    if (from == to) return from ;
+    return from + Sum(from + 1, to);
+}
---
+++ HomeWork_68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычесления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Clear();
+Console.WriteLine("введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат выполнения: {Sum(m, n)}");
+int Sum(int from, int to)
+{
+    if (from == 0) return to + 1;
+    else
+        if ((from != 0) && (to == 0)) return Sum(from - 1, 1);
+    else
+    return Sum(from - 1, Sum(from, to - 1));
+}
+
+
